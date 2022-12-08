3bbf54a4d38a81b143373dc8bbf756e928d9def4
Fathil Arham <fathil.arham@gmail.com>
2022-12-08 12:04:26 +0700

    Add condition validation

--- gin-file-upload-ca/request/request.go
+++ gin-file-upload-ca/request/request.go
@@ -4,5 +4,5 @@ import "mime/multipart"
 
 type Request struct {
 	File      *multipart.FileHeader `form:"file" binding:"required"`
-	Condition string                `form:"condition" binding:"required"`
+	Condition string                `form:"condition" binding:"required,oneof='customer logo'"`
 }
